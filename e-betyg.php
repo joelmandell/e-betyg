92f581ee7897ae8e9a439e112f4ae461ac8c2ee8
Joel Mandell <joelmandell@gmail.com>
2013-09-05 20:27:40 +0200

    Intial

--- e-betyg.php
+++ e-betyg.php
@@ -2,6 +2,63 @@
 /**
  * Documentation, License etc.
  *
+ * License: GNU GPL 3.0
+ *
+ *
+ * Copyright Joel Mandell 2013.
  * @package e-betyg
  */
 
+ //Group-klassen innehåller funktioner för att ge grupptillhörigheter åt inloggad användare.
+ class Group
+ {
+ 
+  function __construct() {
+      print "In BaseClass constructor\n";
+  }
+  
+  public function GetPriviligies()
+  {
+  }
+  
+  public function CheckUser($user)
+  {
+  
+  }
+ 
+ }
+ 
+ class Teacher extends User 
+ {
+ 
+  function __construct() {
+      print "In BaseClass constructor\n";
+  }
+ 
+ }
+ 
+ 
+ class Auth
+ {
+ 
+  
+ 
+ }
+ 
+ class User
+ {
+ 
+  function __construct() {
+      print "In BaseClass constructor\n";
+  }
+   
+  public function Register()
+  {
+  }
+ 
+ } 
+  
+  
+  
+ 
+ }
