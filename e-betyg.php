c48c86eb0cc1b466d955458b7b2d855a87b66f9f
Joel Mandell <joelmandell@gmail.com>
2013-09-06 02:28:09 +0200

    Added class description...

--- e-betyg.php
+++ e-betyg.php
@@ -17,6 +17,12 @@
       print "In BaseClass constructor\n";
   }
   
+  //Lista grupper från databas.
+  public function GetGroups()
+  {
+  
+  }
+  
   public function GetPriviligies()
   {
   }
@@ -28,22 +34,42 @@
  
  }
  
+ /*class Auth.
+ /*
+ /*Detta är en klass som innehåller funktioner för:
+ * datakontroll, inloggningskontroll, omdirigering.
+ * 
+ * 1) Ta emot HTTP POST värden.
+ * 2) Datakontroll - Är fälten rätt ifyllda? Hindra XSS. Ta bort html tags och icke tillåtna tecken.
+ * Om steg 2 innehåller fel så hoppar vi förbi det 3 stycket.
+ * 3) Stämmer inloggningsuppgifter?
+ * 4) OM inloggning är korrekt skicka användaren till användarsidan. OM inte skicka felmeddelande.
+ * OM Datakontroll i steg 2 misslyckades så får användaren också felmeddelande på inloggningssidan.
+ */
+ class Auth
+ {
+ 
+  function __construct() {
+      print "In BaseClass constructor\n";
+  }
+  
+ }
+ 
  class Teacher extends User 
  {
  
   function __construct() {
       print "In BaseClass constructor\n";
   }
- 
- }
- 
- 
- class Auth
- {
- 
   
- 
+   //Ge $value antingen värdet TRUE om konto ska aktiveras eller FALSE om det ska avaktiveras, andra argumentet är kontot som berörs.
+  function Activation(bool $value, $accountId)
+  {
+  
+  }
+  
  }
+ 
  
  class User
  {
@@ -51,13 +77,23 @@
   function __construct() {
       print "In BaseClass constructor\n";
   }
-   
-  public function Register()
+ 
+  //$values är en vektor med användarinmatad information
+  //för att registrera sig - som skickas med en post request via ajax.
+  function Register($values)
   {
+  
+  }
+  
+
+  
+  function Login($values)
+  {
+  
   }
  
- } 
-  
+ }
+ 
   
   
  
