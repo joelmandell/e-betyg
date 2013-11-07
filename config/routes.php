4423e1332ac1dc8ee6ce4401b6271d85d2d4553e
Joel Mandell <joelmandell@gmail.com>
2013-11-08 00:13:48 +0100

    Added some comments on config

--- config/routes.php
+++ config/routes.php
@@ -6,9 +6,15 @@
  * 
  */
 
- define('index_controller','Main');
- define('framework_path','./');
- define('db_pass','');
- define('db_user','root');
- define('db_host','127.0.0.1');
- define('db_charset','utf8');
+//index_controller contains the standard controller that should load
+//when root of page is loaded.
+define('index_controller','Main');
+
+//Framework path - where the library files are.
+define('framework_path','./');
+
+//Some config-settings for database.
+define('db_pass','');
+define('db_user','root');
+define('db_host','127.0.0.1');
+define('db_charset','utf8');
