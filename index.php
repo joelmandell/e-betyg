204d0674ea1a3cc19ebf02e0b7c09b9c9a4fdeff
Joel Mandell <joelmandell@gmail.com>
2013-11-16 00:05:37 +0100

    Removed unecessary $GLOBALS

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 
-require 'config/routes.php';
+session_start();
 
+require 'config/routes.php';
 $framework_path=constant("framework_path");
 
+require $framework_path.'libs/Router.php';
 require $framework_path.'libs/DatabaseConnection.php';
 require $framework_path.'libs/Controller.php';
-require $framework_path.'libs/Router.php';
 require $framework_path.'libs/Model.php';
 require $framework_path.'libs/View.php';
 
 $router = new Router();
+
+
+
