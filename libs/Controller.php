6e27741e7424759c5b334a6c114eda453477542b
Joel Mandell <joelmandell@gmail.com>
2013-11-16 00:08:51 +0100

    Fixed reference to Auth class to follow down the pipe through the classes and the framework

--- libs/Controller.php
+++ libs/Controller.php
@@ -1,15 +1,18 @@
 <?php
 
 //standard methods that get's inherited in all controllers.
+require 'libs/e-betyg.php';
 
 class Controller {
     
-    public $view, $db, $_M;
+    public $view, $db, $_M, $r, $auth;
     
-    function __construct() {
+    function __construct($bundle=NULL) {
+                        
         
         $this->db = new DatabaseConnection("mysql", "e-betyg");
-        
+        $this->auth=new Auth($this->db);
+
         if($_POST)
         {
             //If POST request, do not load view.
@@ -18,7 +21,8 @@ class Controller {
         {
             //There is always a View class available
             //when controller is called as $_GET request
-            $this->view = new View();
+            $this->view = new View($bundle);
+            $this->view->auth=&$this->auth;
             
             //In the View class there is a Model class 
             //we want to make this Model available.
