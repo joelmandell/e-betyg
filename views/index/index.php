aa906a7e930a885018596bd2c99fe0ea2e56eb4a
Joel Mandell <joelmandell@gmail.com>
2013-11-21 22:41:54 +0100

    Added new link in the nav menu

--- views/index/index.php
+++ views/index/index.php
@@ -1,34 +1,60 @@
-<html>
+<!DOCTYPE html>
+<html dir="ltr" lang="sv">
 <head>
     <meta charset="utf-8" /> 
     <link rel="stylesheet" type="text/css" href="/e-betyg/resources/css/main.css" />
     <title>
         E-Betyg
     </title>
-<head>
+</head>
 <body>
     <div id="page">
         <div id="header"><img src="/e-betyg/resources/img/header.png" /></div>
-        <div id="nav"></div>
+        <div id="nav">
+            <ul>
+                
+                <li><a href="#">Start</a></li>
+                <li><a href="Account/Register/">Registrera dig</a></li>
+                
+                <?php
+                    if($user->InvokedPriviligies && $user->GroupName=="ADMIN")
+                    {
+                        echo $_M->edit;
+                    }
+
+                    if($auth->IsAuth())
+                    {
+                        echo $_M->logout;
+                    }
+                ?>
+                
+            </ul>
+        </div>
         
         <div id="text_left_content">
-            <h1>
-                <?php echo $this->_M->h1;?>
-            </h1>
-
-                <?php echo $this->_M->p; ?>
-            <p><a href="Account">Logga in</a></p>
+            <?php echo $_M->h1; ?>
+            <?php echo $_M->p; ?>
         </div>
-        <div id="text_right_content">
-            <h2>Logga in:</h2>
-            <form method="post" action="Account/SignIn" >
-                <label>E-post</label>
-                <input type="text" name="user" />
-                <label>Lösenord</label>
-                <input type="password" name="pass" />
-                <input type="submit" name="submit" value="Logga in" />
-            </form>
-        </div>
+        
+        <div id="text_right_content">          
+            <?php
+            if(!$auth->IsAuth())
+            {
+               echo $_M->login_form; 
+            }
+            ?>
+            <div id="form_msg">
+            <?php
+               echo $auth->IsAuth() ? "" : $_M->msg;            
+            ?>
+            </div>
+        </div>  
     </div>
+    <div id="foot">
+        <div id="foot_content">
+            <a href="http://github.com/joelmandell"><img id="social" src="/e-betyg/resources/img/Octocat.png" /></a><a href="http://plus.google.com/+joelmandell"><img id="social" src="/e-betyg/resources/img/gplus-64.png" /></a><a href="http://twitter.com/dikatlon"><img id="social" src="/e-betyg/resources/img/Twitter_logo_white.png" /></a>
+            <p>Copyright Joel Mandell 2013-2014</p>
+        </div>
+    </div>       
 </body>
 </html>
