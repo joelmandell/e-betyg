e6b27eadf786eecd4df4c39d0c0d3efb41fc235a
Joel Mandell <joelmandell@gmail.com>
2013-11-21 22:45:00 +0100

    Added information to show on account page that user is not logged in

--- views/account/index.php
+++ views/account/index.php
@@ -1,21 +1,51 @@
-<html>
+<!DOCTYPE html>
+<html dir="ltr" lang="sv">
 <head>
     <meta charset="utf-8" /> 
     <link rel="stylesheet" type="text/css" href="/e-betyg/resources/css/main.css" />
     <title>
         E-Betyg
     </title>
-<head>
+</head>
 <body>
     <div id="page">
         <div id="header"><img src="/e-betyg/resources/img/header.png" /></div>
-        <div id="nav"></div>
+        <div id="nav">
+            <ul>
+                
+                <li><a href="/e-betyg/">Start</a></li>
+                <li><a href="#">Registrera</a></li>
+                
+                <?php
+                    if($user->InvokedPriviligies && $user->GroupName=="ADMIN")
+                    {
+                        echo $_M->edit;
+                    }
+
+                    if($auth->IsAuth())
+                    {
+                        echo $_M->logout;
+                    }
+                ?>
+                
+            </ul>
+        </div>
 
         <div id="text_left_content">
-            <h1>Logga in</h1>
             <?php 
-                        
-            echo $_M->hash; ?>
+                echo $_M->h1;
+            ?>
+            <?php 
+                if(!$auth->isAuth())
+                {
+                    echo $_M->not_logged_in;
+                }
+            ?>
+            <p>
+                <?php                      
+                echo $_M->register_success;            
+                ?>
+            </p>
         </div>
         
     </div>
